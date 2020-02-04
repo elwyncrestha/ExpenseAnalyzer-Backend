dae41a43da71fddbcfe07b860014bfe0e0d4105c
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-04 12:46:11 +0545

    Refactor: Remove sub-category and added Expense schema

--- api/category/category.model.js
+++ api/category/category.model.js
@@ -5,15 +5,7 @@ const categorySchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true
-  },
-  subCategory: [
-    {
-      name: {
-        type: String,
-        required: true
-      }
-    }
-  ]
+  }
 });
 
 const Category = mongoose.model("Category", categorySchema);
