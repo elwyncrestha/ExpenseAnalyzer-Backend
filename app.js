baf04b44031b9d3ccda5a7b3fb36676d3e6c3f62
Elvin Shrestha <elwyncrestha@gmail.com>
2020-02-06 19:07:38 +0545

    Refactor: Changed payload size to 200mb

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
 // Routes
 app.use(userRouter);
